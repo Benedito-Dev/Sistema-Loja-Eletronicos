f074374e514e0360ef18aae006578bec136d68ab
Benedito-Dev <beneditobittencourtt@gmail.com>
2024-12-17 19:58:33 -0300

    wip

--- LojaEletronicos/LojaEletronicos/settings.py
+++ LojaEletronicos/LojaEletronicos/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'LojaEletronicos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  # Backend do PostgreSQL
+        'NAME': 'Django',                   # Nome do banco de dados PostgreSQL
+        'USER': 'postgres',                     # Usuário do PostgreSQL
+        'PASSWORD': 'postgres',                   # Senha do usuário
+        'HOST': 'localhost',                       # Endereço do servidor (ou IP)
+        'PORT': '5432',                            # Porta padrão do PostgreSQL
     }
 }
 
